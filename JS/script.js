0bd235b138e3b0ccd51d8c8a9927591fba2fe67b
mizik1 <mattk29@gmail.com>
2024-04-22 14:20:21 -0500

    Fixed brackets
    Fixed brackets so that the first closes after " $("ul").append(li);
    
    The second closes after   "$(this).toggleClass("strike");"
    
    The third closes after " $(this).val("");"
    
    The final is after "  $("ul").sortable();"

--- JS/script.js
+++ JS/script.js
@@ -4,27 +4,29 @@ $(document).ready(function () {
   $("#button").click(function () {
     // Create variable toAdd to process data from input tag (name=ListItem), .val() is used for any value inputted
     let toAdd = $("input[name=ListItem]").val();
-    let li = $("<li>") + toAdd + "</li>");
+    // Create li variable that adds text
+    let li = $("<li></li>").text(toAdd);
 
-  let crossOutButton = $("<crossOutButton></crossOutButton>");
-  crossOutButton.text("X");
+    let crossOutButton = $("<crossOutButton></crossOutButton>");
+    crossOutButton.text("X");
 
-  li.append(crossOutButton);
+    li.append(crossOutButton);
 
-  crossOutButton.on("click", function () {
-    li.addClass("delete");
+    crossOutButton.on("click", function () {
+      li.addClass("delete");
+    });
+
+    $("ul").append(li);
   });
-
-  $('ul').append(li)
-});
   // Remove any 'li' item by double clicking "dblclick"
   $(document).on("dblclick", "li", function () {
     $(this).toggleClass("strike");
   });
+
   // Clear input box after entering item. .focus binds event handler to the focus event
   $("input").focus(function () {
     $(this).val("");
- 
+  });
   // Sorts the "ul" so that any "li" can be reordered
   $("ul").sortable();
 });
